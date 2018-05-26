6b10566781ea73f23bed9af03956e7fdc1cba73f
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-05-26 10:28:10 +0200

    Added parser service

---
+++ src/main/kotlin/berlin/intero/sentientlighthub/common/services/SentientParserService.kt
@@ -0,0 +1,39 @@
+package berlin.intero.sentientlighthub.common.services
+
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.util.logging.Logger
+
+object SentientParserService {
+
+    private val log: Logger = Logger.getLogger(SentientParserService::class.simpleName)
+
+    /**
+     * Parses a byte array into a string
+     */
+    fun parse(byteArray: ByteArray): ArrayList<String> {
+        val parsedValues = ArrayList<String>()
+
+        val bytes: MutableList<Byte> = ArrayList()
+        val it = byteArray.iterator()
+
+        while (it.hasNext()) {
+            bytes.add(it.nextByte())
+        }
+
+        for (i in 0 until bytes.size step 2) {
+            val bytesArray = ByteArray(2)
+            bytesArray[0] = bytes[i]
+            bytesArray[1] = bytes[i + 1]
+
+            val int16 = ByteBuffer.wrap(bytesArray).order(ByteOrder.LITTLE_ENDIAN).short
+            log.fine("Parsed value $int16")
+
+            parsedValues.add(int16.toString())
+        }
+
+        log.fine("Parsed value ${parsedValues.size}")
+
+        return parsedValues
+    }
+}
