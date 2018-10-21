16a102d60e9ab37465fa233e28748db5d3afc543
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-10-21 19:28:04 +0200

    Added iCE stick serial connector to list of devices

--- src/main/kotlin/berlin/intero/sentientlighthub/common/model/actor/ActorDevice.kt
+++ src/main/kotlin/berlin/intero/sentientlighthub/common/model/actor/ActorDevice.kt
@@ -2,6 +2,7 @@ package berlin.intero.sentientlighthub.common.model.actor
 
 data class ActorDevice(
         val address: String,
+        val port: String,
         val owner: String,
         val description: String,
         val location: String,
