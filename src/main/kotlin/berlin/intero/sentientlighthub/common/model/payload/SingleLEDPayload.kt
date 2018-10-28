b1234545ad79ac1f19fc87ce40177ad89790b31e
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-10-28 22:34:37 +0100

    Updated protocol for single LED

--- src/main/kotlin/berlin/intero/sentientlighthub/common/model/payload/SingleLEDPayload.kt
+++ src/main/kotlin/berlin/intero/sentientlighthub/common/model/payload/SingleLEDPayload.kt
@@ -1,3 +1,9 @@
 package berlin.intero.sentientlighthub.common.model.payload
 
-data class SingleLEDPayload(val warmWhite: String, val coldWhite: String, val amber: String)
+data class SingleLEDPayload(
+        val stripId: String,
+        val ledId: String,
+        val warmWhite: String,
+        val coldWhite: String,
+        val amber: String
+)
